090a4f45f59aa32dc0727530fc689beb3c701c72
EspejelKevin <kevin.espejelmtz@gmail.com>
2026-07-11 08:18:10 -0600

    feature: desarrollo de la configuracion del framework spring boot security

---
+++ workout-tracker/src/main/java/com/workouttracker/shared/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.workouttracker.shared.config;
+
+import com.workouttracker.application.auth.UserDetailsImpl;
+import com.workouttracker.shared.filters.JwtFilter;
+import com.workouttracker.shared.jwt.JwtUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final JwtUtils jwtUtils;
+    private final UserDetailsImpl userDetailsImpl;
+
+    public SecurityConfig(JwtUtils jwtUtils, UserDetailsImpl userDetailsImpl) {
+        this.jwtUtils = jwtUtils;
+        this.userDetailsImpl = userDetailsImpl;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(HttpMethod.POST, "/security-management/**").permitAll();
+                    auth.anyRequest().hasRole("REGULAR_USER");
+                })
+                .addFilterBefore(new JwtFilter(this.jwtUtils), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(this.userDetailsImpl);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
+        return config.getAuthenticationManager();
+    }
+}
